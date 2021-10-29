0bd6493d08c9769472b55420e7ad54d7f501703d
ym2 <a01570056@itesm.mx>
2021-10-29 08:37:40 -0500

    Menu to introimages

--- BridgeTech/Assets/Scripts/MenuPrincipal.cs
+++ BridgeTech/Assets/Scripts/MenuPrincipal.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class MenuPrincipal : MonoBehaviour
 {
     public void PlayGame(){
-    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    	// SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   		SceneManager.LoadScene("IntroImages");
     }
 
     public void QuitGame(){
